5bcde6c6894a87fed52978b9cfe2c33253efd955
Musarrat Jahan <jahan786musarrat.786@gmail.com>
2026-06-16 09:12:27 +0530

    Exceptions in Python

---
+++ basics/06_Excepetion_Handling/03_raising_an_excpn.py
@@ -0,0 +1,20 @@
+# num=-2
+# if num<0:
+#     raise Exception("Negative numbers not allowed")
+
+# a=5
+# if a%2!=0:
+#     raise Exception ("The number shouldn't be odd")
+
+# s='apple'
+# try:
+#     num=int(s)
+# except ValueError:
+#     raise ValueError("String can't be changed to integer")
+
+
+s='apple'
+try:
+    num=int(s)
+except:
+    raise 
